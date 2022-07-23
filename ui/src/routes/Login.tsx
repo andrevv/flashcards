a37dae586106edd43f4e299cd5ae4a0c62ce2339
andrevv <andrey.vladykov@gmail.com>
2022-07-23 18:45:13 +0300

    Add login form validation.

--- ui/src/routes/Login.tsx
+++ ui/src/routes/Login.tsx
@@ -2,12 +2,13 @@ import { useContext, useState } from "react";
 import { ExclamationCircleIcon } from "@heroicons/react/outline";
 import AuthContext from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
-import { Field, Form, Formik, FormikHelpers } from "formik";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
-interface Values {
-  email: string;
-  password: string;
-}
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
 
 export default function Login() {
   const [error, setError] = useState<string>();
@@ -44,54 +45,63 @@ export default function Login() {
               )}
               <Formik
                 initialValues={{ email: "", password: "" }}
-                onSubmit={async (
-                  { email, password }: Values,
-                  { setSubmitting }: FormikHelpers<Values>
-                ) => {
+                validationSchema={LoginSchema}
+                onSubmit={async ({ email, password }) => {
                   await auth.signIn(email, password, () => {
                     navigate("/");
-                    setSubmitting(false);
                   });
                 }}
               >
-                <Form action="" className="mt-12">
-                  <div className="relative">
-                    <Field
-                      type="text"
-                      id="email"
-                      name="email"
-                      className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent"
-                      placeholder="Email address"
-                    />
-                    <label
-                      htmlFor="email"
-                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm cursor-text"
+                {({ errors, touched }) => (
+                  <Form className="mt-12">
+                    <div className="relative">
+                      <Field
+                        type="text"
+                        id="email"
+                        name="email"
+                        className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent"
+                        placeholder="Email address"
+                      />
+                      {errors.email && touched.email ? (
+                        <div className="text-red-700 font-semibold opacity-90 py-2">
+                          {errors.email}
+                        </div>
+                      ) : null}
+                      <label
+                        htmlFor="email"
+                        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm cursor-text"
+                      >
+                        Email address
+                      </label>
+                    </div>
+                    <div className="mt-10 relative">
+                      <Field
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent"
+                        placeholder="Password"
+                      />
+                      {errors.password && touched.password ? (
+                        <div className="text-red-700 font-semibold opacity-90 py-2">
+                          {errors.password}
+                        </div>
+                      ) : null}
+                      <label
+                        htmlFor="password"
+                        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm cursor-text"
+                      >
+                        Password
+                      </label>
+                    </div>
+                    <button
+                      type="submit"
+                      className="mt-20 px-4 py-2 rounded bg-rose-500 hover:bg-rose-400 text-white font-semibold text-center block w-full focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-50 cursor-pointer"
                     >
-                      Email address
-                    </label>
-                  </div>
-                  <div className="mt-10 relative">
-                    <Field
-                      type="password"
-                      id="password"
-                      name="password"
-                      className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 placeholder-transparent"
-                      placeholder="Password"
-                    />
-                    <label
-                      htmlFor="password"
-                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm cursor-text"
-                    >
-                      Password
-                    </label>
-                  </div>
-                  <button
-                    type="submit"
-                    className="mt-20 px-4 py-2 rounded bg-rose-500 hover:bg-rose-400 text-white font-semibold text-center block w-full focus:outline-none focus:ring focus:ring-offset-2 focus:ring-rose-500 focus:ring-opacity-50 cursor-pointer"
-                  >
-                    Sign In
-                  </button>
-                </Form>
+                      Sign In
+                    </button>
+                  </Form>
+                )}
               </Formik>
             </div>
           </div>
